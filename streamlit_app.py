0f28ae33e7c4ecb0b15357333788902241f0f522
yanwm115 <yanwm@bc.edu>
2025-06-13 21:22:54 +0000

    deleted vega_dataset

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-from vega_datasets import data
 
 df = pd.read_csv("Massachusetts Food Access Data - Sheet1.csv")
 st.set_page_config(layout="wide")
